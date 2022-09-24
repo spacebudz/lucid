2966fbf4c5dbca761ab7c4c4e245ef97a8c7a6e6
Riley-Kilgore <Riley_Kilgore@outlook.com>
2022-09-23 19:43:42 -0700

    I think that this needs a change..

--- src/plutus/time.ts
+++ src/plutus/time.ts
@@ -1,7 +1,7 @@
 import { Network, Slot, SlotConfig, UnixTime } from "../types/mod.ts";
 
 export const zeroTimeNetwork: Record<Network, UnixTime> = {
-  Mainnet: 1596059091000, // Shelley start (slotLength = 1s).
+  Mainnet: 1596049091000, // Shelley start (slotLength = 1s).
   Testnet: 1595967616000, // Shelley start (slotLength = 1s).
   Preview: 1660003200000, // Genesis start (slotLength = 1s).
   Preprod: 1654041600000, // Genesis start (slotLength = 1s).
