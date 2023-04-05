30ad4b5c3042bc0bb320a18df3756b936c272695
alessandrokonrad <akonrad6@gmail.com>
2023-04-05 12:13:49 +0200

    Fixed ssr

--- build.ts
+++ build.ts
@@ -99,20 +99,36 @@ if (isNode) {
     if (!globalThis.fs) globalThis.fs = fs; 
 }
 
-const C = isNode
-  ? await import(
-    /* webpackIgnore: true */ "./libs/cardano_multiplatform_lib/nodejs/cardano_multiplatform_lib.generated.js"
-  )
-  : await import(
-    "./libs/cardano_multiplatform_lib/cardano_multiplatform_lib.generated.js"
-  );
-const M = isNode
-  ? await import(
-    /* webpackIgnore: true */ "./libs/cardano_message_signing/nodejs/cardano_message_signing.generated.js"
-  )
-  : await import(
-    "./libs/cardano_message_signing/cardano_message_signing.generated.js"
-  );
+const C = await (async () => {
+  try {
+    if (isNode) {
+      return await import(
+        /* webpackIgnore: true */ "./libs/cardano_multiplatform_lib/nodejs/cardano_multiplatform_lib.generated.js"
+      );
+    }
+    return await import(
+      "./libs/cardano_multiplatform_lib/cardano_multiplatform_lib.generated.js"
+    );
+  } catch (_e) {
+    // This only ever happens during SSR rendering
+    return null;
+  }
+})();
+const M = await (async () => {
+  try {
+    if (isNode) {
+      return await import(
+        /* webpackIgnore: true */ "./libs/cardano_message_signing/nodejs/cardano_message_signing.generated.js"
+      );
+    }
+    return await import(
+      "./libs/cardano_message_signing/cardano_message_signing.generated.js"
+    );
+  } catch (_e) {
+    // This only ever happens during SSR rendering
+    return null;
+  }
+})();
 if (!isNode) {
   async function unsafeInstantiate(module) {
     try {
