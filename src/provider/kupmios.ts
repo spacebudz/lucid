1fe855782127660ff7fb6b66f01bbbc1cd26e748
yHSJ <jgm0100@mcdaniel.edu>
2023-11-04 17:18:44 -0400

    feat manage memory in kupmios.kupmiosUtxosToUtxos

--- src/provider/kupmios.ts
+++ src/provider/kupmios.ts
@@ -36,22 +36,23 @@ export class Kupmios implements Provider {
     });
 
     return new Promise((res, rej) => {
-      client.addEventListener("message", (msg: MessageEvent<string>) => {
-        try {
-          const { result } = JSON.parse(msg.data);
+      client.addEventListener(
+        "message",
+        (msg: MessageEvent<string>) => {
+          try {
+            const { result } = JSON.parse(msg.data);
 
-          // deno-lint-ignore no-explicit-any
-          const costModels: any = {};
-          Object.keys(result.costModels).forEach((v) => {
-            const version = v.split(":")[1].toUpperCase();
-            const plutusVersion = "Plutus" + version;
-            costModels[plutusVersion] = result.costModels[v];
-          });
-          const [memNum, memDenom] = result.prices.memory.split("/");
-          const [stepsNum, stepsDenom] = result.prices.steps.split("/");
+            // deno-lint-ignore no-explicit-any
+            const costModels: any = {};
+            Object.keys(result.costModels).forEach((v) => {
+              const version = v.split(":")[1].toUpperCase();
+              const plutusVersion = "Plutus" + version;
+              costModels[plutusVersion] = result.costModels[v];
+            });
+            const [memNum, memDenom] = result.prices.memory.split("/");
+            const [stepsNum, stepsDenom] = result.prices.steps.split("/");
 
-          res(
-            {
+            res({
               minFeeA: parseInt(result.minFeeCoefficient),
               minFeeB: parseInt(result.minFeeConstant),
               maxTxSize: parseInt(result.maxTxSize),
@@ -62,19 +63,20 @@ export class Kupmios implements Provider {
               priceStep: parseInt(stepsNum) / parseInt(stepsDenom),
               maxTxExMem: BigInt(result.maxExecutionUnitsPerTransaction.memory),
               maxTxExSteps: BigInt(
-                result.maxExecutionUnitsPerTransaction.steps,
+                result.maxExecutionUnitsPerTransaction.steps
               ),
               coinsPerUtxoByte: BigInt(result.coinsPerUtxoByte),
               collateralPercentage: parseInt(result.collateralPercentage),
               maxCollateralInputs: parseInt(result.maxCollateralInputs),
               costModels,
-            },
-          );
-          client.close();
-        } catch (e) {
-          rej(e);
-        }
-      }, { once: true });
+            });
+            client.close();
+          } catch (e) {
+            rej(e);
+          }
+        },
+        { once: true }
+      );
     });
   }
 
@@ -86,15 +88,14 @@ export class Kupmios implements Provider {
     const result = await fetch(
       `${this.kupoUrl}/matches/${queryPredicate}${
         isAddress ? "" : "/*"
-      }?unspent`,
-    )
-      .then((res) => res.json());
+      }?unspent`
+    ).then((res) => res.json());
     return this.kupmiosUtxosToUtxos(result);
   }
 
   async getUtxosWithUnit(
     addressOrCredential: Address | Credential,
-    unit: Unit,
+    unit: Unit
   ): Promise<UTxO[]> {
     const isAddress = typeof addressOrCredential === "string";
     const queryPredicate = isAddress
@@ -106,9 +107,8 @@ export class Kupmios implements Provider {
         isAddress ? "" : "/*"
       }?unspent&policy_id=${policyId}${
         assetName ? `&asset_name=${assetName}` : ""
-      }`,
-    )
-      .then((res) => res.json());
+      }`
+    ).then((res) => res.json());
     return this.kupmiosUtxosToUtxos(result);
   }
 
@@ -117,9 +117,8 @@ export class Kupmios implements Provider {
     const result = await fetch(
       `${this.kupoUrl}/matches/${policyId}.${
         assetName ? `${assetName}` : "*"
-      }?unspent`,
-    )
-      .then((res) => res.json());
+      }?unspent`
+    ).then((res) => res.json());
 
     const utxos = await this.kupmiosUtxosToUtxos(result);
 
@@ -133,51 +132,59 @@ export class Kupmios implements Provider {
   async getUtxosByOutRef(outRefs: Array<OutRef>): Promise<UTxO[]> {
     const queryHashes = [...new Set(outRefs.map((outRef) => outRef.txHash))];
 
-    const utxos = await Promise.all(queryHashes.map(async (txHash) => {
-      const result = await fetch(
-        `${this.kupoUrl}/matches/*@${txHash}?unspent`,
-      ).then((res) => res.json());
-      return this.kupmiosUtxosToUtxos(result);
-    }));
-
-    return utxos.reduce((acc, utxos) => acc.concat(utxos), []).filter((utxo) =>
-      outRefs.some((outRef) =>
-        utxo.txHash === outRef.txHash && utxo.outputIndex === outRef.outputIndex
-      )
+    const utxos = await Promise.all(
+      queryHashes.map(async (txHash) => {
+        const result = await fetch(
+          `${this.kupoUrl}/matches/*@${txHash}?unspent`
+        ).then((res) => res.json());
+        return this.kupmiosUtxosToUtxos(result);
+      })
     );
+
+    return utxos
+      .reduce((acc, utxos) => acc.concat(utxos), [])
+      .filter((utxo) =>
+        outRefs.some(
+          (outRef) =>
+            utxo.txHash === outRef.txHash &&
+            utxo.outputIndex === outRef.outputIndex
+        )
+      );
   }
 
   async getDelegation(rewardAddress: RewardAddress): Promise<Delegation> {
     const client = await this.ogmiosWsp("Query", {
-      query: { "delegationsAndRewards": [rewardAddress] },
+      query: { delegationsAndRewards: [rewardAddress] },
     });
 
     return new Promise((res, rej) => {
-      client.addEventListener("message", (msg: MessageEvent<string>) => {
-        try {
-          const { result } = JSON.parse(msg.data);
-          const delegation = (result ? Object.values(result)[0] : {}) as {
-            delegate: string;
-            rewards: number;
-          };
-          res(
-            {
+      client.addEventListener(
+        "message",
+        (msg: MessageEvent<string>) => {
+          try {
+            const { result } = JSON.parse(msg.data);
+            const delegation = (result ? Object.values(result)[0] : {}) as {
+              delegate: string;
+              rewards: number;
+            };
+            res({
               poolId: delegation?.delegate || null,
               rewards: BigInt(delegation?.rewards || 0),
-            },
-          );
-          client.close();
-        } catch (e) {
-          rej(e);
-        }
-      }, { once: true });
+            });
+            client.close();
+          } catch (e) {
+            rej(e);
+          }
+        },
+        { once: true }
+      );
     });
   }
 
   async getDatum(datumHash: DatumHash): Promise<Datum> {
-    const result = await fetch(
-      `${this.kupoUrl}/datums/${datumHash}`,
-    ).then((res) => res.json());
+    const result = await fetch(`${this.kupoUrl}/datums/${datumHash}`).then(
+      (res) => res.json()
+    );
     if (!result || !result.datum) {
       throw new Error(`No datum found for datum hash: ${datumHash}`);
     }
@@ -188,7 +195,7 @@ export class Kupmios implements Provider {
     return new Promise((res) => {
       const confirmation = setInterval(async () => {
         const isConfirmed = await fetch(
-          `${this.kupoUrl}/matches/*@${txHash}?unspent`,
+          `${this.kupoUrl}/matches/*@${txHash}?unspent`
         ).then((res) => res.json());
         if (isConfirmed && isConfirmed.length > 0) {
           clearInterval(confirmation);
@@ -205,80 +212,94 @@ export class Kupmios implements Provider {
     });
 
     return new Promise((res, rej) => {
-      client.addEventListener("message", (msg: MessageEvent<string>) => {
-        try {
-          const { result } = JSON.parse(msg.data);
+      client.addEventListener(
+        "message",
+        (msg: MessageEvent<string>) => {
+          try {
+            const { result } = JSON.parse(msg.data);
 
-          if (result.SubmitSuccess) res(result.SubmitSuccess.txId);
-          else rej(result.SubmitFail);
-          client.close();
-        } catch (e) {
-          rej(e);
-        }
-      }, { once: true });
+            if (result.SubmitSuccess) res(result.SubmitSuccess.txId);
+            else rej(result.SubmitFail);
+            client.close();
+          } catch (e) {
+            rej(e);
+          }
+        },
+        { once: true }
+      );
     });
   }
 
   private kupmiosUtxosToUtxos(utxos: unknown): Promise<UTxO[]> {
-    // deno-lint-ignore no-explicit-any
-    return Promise.all((utxos as any).map(async (utxo: any) => {
-      return ({
-        txHash: utxo.transaction_id,
-        outputIndex: parseInt(utxo.output_index),
-        address: utxo.address,
-        assets: (() => {
-          const a: Assets = { lovelace: BigInt(utxo.value.coins) };
-          Object.keys(utxo.value.assets).forEach((unit) => {
-            a[unit.replace(".", "")] = BigInt(utxo.value.assets[unit]);
-          });
-          return a;
-        })(),
-        datumHash: utxo?.datum_type === "hash" ? utxo.datum_hash : null,
-        datum: utxo?.datum_type === "inline"
-          ? await this.getDatum(utxo.datum_hash)
-          : null,
-        scriptRef: utxo.script_hash &&
-          (await (async () => {
-            const {
-              script,
-              language,
-            } = await fetch(
-              `${this.kupoUrl}/scripts/${utxo.script_hash}`,
-            ).then((res) => res.json());
+    return Promise.all(
+      // deno-lint-ignore no-explicit-any
+      (utxos as any).map(async (utxo: any) => {
+        return {
+          txHash: utxo.transaction_id,
+          outputIndex: parseInt(utxo.output_index),
+          address: utxo.address,
+          assets: (() => {
+            const a: Assets = { lovelace: BigInt(utxo.value.coins) };
+            Object.keys(utxo.value.assets).forEach((unit) => {
+              a[unit.replace(".", "")] = BigInt(utxo.value.assets[unit]);
+            });
+            return a;
+          })(),
+          datumHash: utxo?.datum_type === "hash" ? utxo.datum_hash : null,
+          datum:
+            utxo?.datum_type === "inline"
+              ? await this.getDatum(utxo.datum_hash)
+              : null,
+          scriptRef:
+            utxo.script_hash &&
+            (await (async () => {
+              const { script, language } = await fetch(
+                `${this.kupoUrl}/scripts/${utxo.script_hash}`
+              ).then((res) => res.json());
 
-            if (language === "native") {
-              return { type: "Native", script };
-            } else if (language === "plutus:v1") {
-              return {
-                type: "PlutusV1",
-                script: toHex(C.PlutusScript.new(fromHex(script)).to_bytes()),
-              };
-            } else if (language === "plutus:v2") {
-              return {
-                type: "PlutusV2",
-                script: toHex(C.PlutusScript.new(fromHex(script)).to_bytes()),
-              };
-            }
-          })()),
-      }) as UTxO;
-    }));
+              if (language === "native") {
+                return { type: "Native", script };
+              } else if (language === "plutus:v1") {
+                const plutusScript = C.PlutusScript.new(fromHex(script));
+                const scriptBytes = plutusScript.to_bytes();
+                plutusScript.free();
+                return {
+                  type: "PlutusV1",
+                  script: toHex(scriptBytes),
+                };
+              } else if (language === "plutus:v2") {
+                const plutusScript = C.PlutusScript.new(fromHex(script));
+                const scriptBytes = plutusScript.to_bytes();
+                plutusScript.free();
+
+                return {
+                  type: "PlutusV2",
+                  script: toHex(scriptBytes),
+                };
+              }
+            })()),
+        } as UTxO;
+      })
+    );
   }
 
   private async ogmiosWsp(
     methodname: string,
-    args: unknown,
+    args: unknown
   ): Promise<WebSocket> {
     const client = new WebSocket(this.ogmiosUrl);
     await new Promise((res) => {
       client.addEventListener("open", () => res(1), { once: true });
     });
-    client.send(JSON.stringify({
-      type: "jsonwsp/request",
-      version: "1.0",
-      servicename: "ogmios",
-      methodname,
-      args,
-    }));
+    client.send(
+      JSON.stringify({
+        type: "jsonwsp/request",
+        version: "1.0",
+        servicename: "ogmios",
+        methodname,
+        args,
+      })
+    );
     return client;
   }
 }
