4983c21a5fa7e13f94b5944bc5099fea6e30d419
alessandrokonrad <akonrad6@gmail.com>
2025-03-24 16:23:49 +0100

    reversed to sync sha256

--- lib/utils/merkle_tree.ts
+++ lib/utils/merkle_tree.ts
@@ -1,6 +1,6 @@
 // Haskell implementation: https://github.com/input-output-hk/hydra-poc/blob/master/plutus-merkle-tree/src/Plutus/MerkleTree.hs
-// TODO: get rid of async sha256
 import { concat, equals } from "jsr:@std/bytes@1.0.4";
+import { crypto } from "jsr:@std/crypto@1.0.4";
 import { toHex } from "./utils.ts";
 
 type MerkleNode = {
@@ -11,33 +11,27 @@ type MerkleNode = {
 
 type Hash = Uint8Array;
 
-type MerkleTreeProof = Array<{
+type MerkleProof = Array<{
   left?: Hash;
   right?: Hash;
 }>;
 
 export class MerkleTree {
-  root!: MerkleNode | null;
+  private root: MerkleNode | null;
 
   /** Construct Merkle tree from data, which get hashed with sha256 */
-  static async new(data: Array<Uint8Array>): Promise<MerkleTree> {
-    const tree = new this();
-    tree.root = await MerkleTree.buildRecursively(
-      await Promise.all(data.map((d) => sha256(d))),
-    );
-    return tree;
+  constructor(data: Uint8Array[]) {
+    this.root = this.build(data.map(sha256));
   }
 
   /** Construct Merkle tree from sha256 hashes */
-  static async fromHashes(hashes: Array<Hash>): Promise<MerkleTree> {
-    const tree = new this();
-    tree.root = await MerkleTree.buildRecursively(hashes);
-    return tree;
+  static fromHashes(hashes: Hash[]): MerkleTree {
+    return new this(hashes);
   }
 
-  private static async buildRecursively(
-    hashes: Array<Hash>,
-  ): Promise<MerkleNode | null> {
+  private build(
+    hashes: Hash[],
+  ): MerkleNode | null {
     if (hashes.length <= 0) return null;
     if (hashes.length === 1) {
       return {
@@ -49,11 +43,13 @@ export class MerkleTree {
 
     const cutoff = Math.floor(hashes.length / 2);
     const [left, right] = [hashes.slice(0, cutoff), hashes.slice(cutoff)];
-    const lnode = await this.buildRecursively(left);
-    const rnode = await this.buildRecursively(right);
+    const lnode = this.build(left);
+    const rnode = this.build(right);
+
     if (lnode === null || rnode === null) return null;
+
     return {
-      node: await combineHash(lnode.node, rnode.node),
+      node: combineHash(lnode.node, rnode.node),
       left: lnode,
       right: rnode,
     };
@@ -64,81 +60,85 @@ export class MerkleTree {
     return this.root.node;
   }
 
-  async getProof(data: Uint8Array): Promise<MerkleTreeProof> {
-    const hash = await sha256(data);
-    const proof: MerkleTreeProof = [];
-    const searchRecursively = (tree: MerkleNode | null) => {
+  getProof(data: Uint8Array): MerkleProof {
+    const proof: MerkleProof = [];
+
+    function search(tree: MerkleNode | null): boolean {
       if (tree && equals(tree.node, hash)) return true;
 
       if (tree?.right) {
-        if (searchRecursively(tree.left)) {
+        if (search(tree.left)) {
           proof.push({ right: tree.right.node });
           return true;
         }
       }
 
       if (tree?.left) {
-        if (searchRecursively(tree.right)) {
+        if (search(tree.right)) {
           proof.push({ left: tree.left.node });
           return true;
         }
       }
-    };
-    searchRecursively(this.root);
+
+      return false;
+    }
+
+    const hash = sha256(data);
+    search(this.root);
     return proof;
   }
 
   size(): number {
-    const searchRecursively = (tree: MerkleNode | null): number => {
+    function search(tree: MerkleNode | null): number {
       if (tree === null) return 0;
-      return 1 + searchRecursively(tree.left) + searchRecursively(tree.right);
-    };
-    return searchRecursively(this.root);
+      return 1 + search(tree.left) + search(tree.right);
+    }
+    return search(this.root);
   }
 
-  static async verify(
+  static verify(
     data: Uint8Array,
     rootHash: Hash,
-    proof: MerkleTreeProof,
-  ): Promise<boolean> {
-    const hash = await sha256(data);
-    const searchRecursively = async (
+    proof: MerkleProof,
+  ): boolean {
+    function search(
       rootHash2: Hash,
-      proof: MerkleTreeProof,
-    ): Promise<boolean> => {
+      proof: MerkleProof,
+    ): boolean {
       if (proof.length <= 0) return equals(rootHash, rootHash2);
       const [h, t] = [proof[0], proof.slice(1)];
       if (h.left) {
-        return searchRecursively(await combineHash(h.left, rootHash2), t);
+        return search(combineHash(h.left, rootHash2), t);
       }
       if (h.right) {
-        return searchRecursively(await combineHash(rootHash2, h.right), t);
+        return search(combineHash(rootHash2, h.right), t);
       }
       return false;
-    };
-    return searchRecursively(hash, proof);
+    }
+
+    const hash = sha256(data);
+    return search(hash, proof);
   }
 
   toString(): string {
-    // deno-lint-ignore no-explicit-any
-    const searchRecursively = (tree: MerkleNode | null): any => {
+    function search(tree: MerkleNode | null): unknown {
       if (tree === null) return null;
       return {
         node: toHex(tree.node),
-        left: searchRecursively(tree.left),
-        right: searchRecursively(tree.right),
+        left: search(tree.left),
+        right: search(tree.right),
       };
-    };
-    return JSON.stringify(searchRecursively(this.root), null, 2);
+    }
+    return JSON.stringify(search(this.root), null, 2);
   }
 }
 
 export { concat, equals };
 
-export async function sha256(data: Uint8Array): Promise<Hash> {
-  return new Uint8Array(await crypto.subtle.digest("SHA-256", data));
+export function sha256(data: Uint8Array): Hash {
+  return new Uint8Array(crypto.subtle.digestSync("SHA-256", data));
 }
 
-export function combineHash(hash1: Hash, hash2: Hash): Promise<Hash> {
+export function combineHash(hash1: Hash, hash2: Hash): Hash {
   return sha256(concat([hash1, hash2]));
 }
